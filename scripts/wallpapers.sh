789c3ca6b1b7adf8562c4dba7193bb2b2f018d8a
Daniel Svens <danielsvens@hotmail.com>
2026-02-03 07:55:50 +0100

    Latest cfg

--- scripts/wallpapers.sh
+++ scripts/wallpapers.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 directory=~/Documents/wallpaper/
-monitor=$(hyprctl monitors | grep Monitor | awk '{print $2}')
 
-if [ -d "$directory" ]; then
-  random_background=$(find $directory/*.jpg | shuf -n 1)
+for monitor in $(hyprctl monitors | grep 'Monitor' | awk '{ print $2 }'); do
 
-  hyprctl hyprpaper unload all
-  hyprctl hyprpaper preload "$random_background"
-  hyprctl hyprpaper wallpaper "$monitor, $random_background"
+  if [ -d "$directory" ]; then
+    random_background=$(find $directory/*.jpg | shuf -n 1)
 
-fi
+    #hyprctl hyprpaper unload all
+    #hyprctl hyprpaper preload "$random_background"
+    hyprctl hyprpaper wallpaper "$monitor,$random_background"
+
+  fi
+
+done
